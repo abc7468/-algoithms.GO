576c2cfe519c73a8fe270709a25b54bd5ec94e22
abc7468 <abc7468@naver.com>
2021-09-13 15:48:44 +0900

    입국 탐색 : 이분 탐색

---
+++ Programmers.Lv3/Q43238/Q43238.go
@@ -0,0 +1,40 @@
+package q43238
+
+import (
+	"fmt"
+	"sort"
+)
+
+func binarySearch(n int, times []int) int {
+	sort.Ints(times)
+	left := 1
+	right := n * times[len(times)-1]
+	answer := 0
+	for left < right {
+		middle := ((left + right) / 2)
+		tmp := 0
+		for _, val := range times {
+			tmp += middle / val
+		}
+		if tmp < n {
+			left = middle + 1
+		} else if tmp >= n {
+			answer = middle
+
+			right = middle - 1
+		} else {
+			break
+		}
+
+	}
+	return int64(answer)
+}
+
+func solution(n int, times []int) int64 {
+	return binarySearch(n, times)
+}
+
+func Start() {
+	times := []int{7, 10}
+	fmt.Println(solution(6, times))
+}
